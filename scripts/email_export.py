eb414ad2eddebeaafdb96af83d20863ae78f3897
chrischcodes <imchrismith@gmail.com>
2025-08-07 06:02:58 -0500

    chore: rename from gmail archive to gmail sync

--- scripts/email_export.py
+++ scripts/email_export.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Run the HigherDOSE Gmail full archive."""
-from higherdose.mail import gmail_archive
+from higherdose.mail import gmail_sync
 
 if __name__ == "__main__":
-    gmail_archive.main()
+    gmail_sync.main()
